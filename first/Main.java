38bafd26c8202024c5816d05c56296b328618606
Nomko <nomkopolopo@gmail.com>
2026-02-19 16:16:23 +0400

    first commit

---
+++ first/Main.java
@@ -0,0 +1,10 @@
+// import packages.*;
+import packages.functions.TabulatedFunction;
+
+public class Main {
+    public static void main(String[] args) {
+        TabulatedFunction tabf = new TabulatedFunction(-5.0, 5.0, 10);
+        System.out.println(tabf);
+    }
+}
+
---
+++ first/packages/functions.java
@@ -0,0 +1,54 @@
+package packages;
+
+import java.util.ArrayList;
+
+public class functions {
+    public static class FunctionPoint {
+        private double x, y;
+
+        FunctionPoint(double _x, double _y) {
+            x = _x;
+            y = _y;
+        }
+
+        FunctionPoint(FunctionPoint point) {
+            x = point.x;
+            y = point.y;
+        }
+
+        FunctionPoint() {
+            x = 0;
+            y = 0;
+        }
+        @Override
+        public String toString() {
+            return String.format("(%.2f; %.2f)", x, y);
+        }
+    }
+    public static class TabulatedFunction {
+        ArrayList<FunctionPoint> points = new ArrayList<>();
+
+        public TabulatedFunction(double leftX, double rightX, int pointsCount) {
+            double step = (rightX - leftX) / (pointsCount - 1);
+            for (int i = 0; i < pointsCount; i++) {
+                FunctionPoint p = new FunctionPoint(leftX + i * step, 0);
+                points.add(p);
+            }
+        }
+        
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder("Tabulated Func:[");
+            for (int i = 0; i < points.size(); i++) {
+                sb.append(points.get(i).toString());
+                if (i < points.size() - 1)
+                    sb.append(", ");
+            }
+            sb.append("]");
+            return sb.toString();
+        }
+    }
+    
+}
+
+
