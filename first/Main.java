9db4ec809356f1b7bd90acf3d4dbef6c0b5b35a9
Nomko <nomkopolopo@gmail.com>
2026-02-20 00:20:50 +0400

    переименовка

--- first/Main.java
+++ first/Main.java
@@ -5,19 +5,17 @@ import packages.functions.TabulatedFunction;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanf = new Scanner(System.in);
-        
+        Scanner scanner = new Scanner(System.in);
         double[] values = new double[5];
         for (int i = 0; i < values.length - 1; i++) {
             System.out.print("Введите элемент[" + i + "]: ");
-            values[i] = scanf.nextDouble();
+            values[i] = scanner.nextDouble();
         }
 
         TabulatedFunction tabf = new TabulatedFunction(0, 10.0, values);
         System.out.println(tabf);
 
-
-        scanf.close();
+        scanner.close();
     }
 }
 
