c65266c0544f78bd0506bf695cfd8c1175e6a486
Nomko <nomkopolopo@gmail.com>
2026-03-05 18:08:35 +0400

    убрал лишний импорт

--- first/packages/TabulatedFunction.java
+++ first/packages/TabulatedFunction.java
@@ -1,7 +1,5 @@
 package packages;
 
-import java.util.ArrayList;
-
 public class TabulatedFunction {
     
     private FunctionPoint points[];
@@ -12,7 +10,8 @@ public class TabulatedFunction {
         leftX_ = leftX;
         rightX_ = rightX;
         pointsCount_ = pointsCount;
-        
+        points = new FunctionPoint[pointsCount];
+
         double step = (rightX - leftX) / (pointsCount - 1);
         for (int i = 0; i < pointsCount_; i++) {
             FunctionPoint p = new FunctionPoint(leftX + i * step, 0);
@@ -42,92 +41,118 @@ public class TabulatedFunction {
     }
 
     public double getFunctionalvalue(double x) {
-        if (x > leftX_ && x < rightX_) {
-            for (int i = 0; i < points.size() - 1; i++) {
-                FunctionPoint p1 = points.get(i);
-                FunctionPoint p2 = points.get(i + 1);
-                if (x > p1.y && x < p2.x) {
+        if (x >= leftX_ && x <= rightX_) {
+            for (int i = 0; i < points.length - 1; i++) {
+                FunctionPoint p1 = points[i];
+                FunctionPoint p2 = points[i + 1];
+                if (x >= p1.x && x <= p2.x) {
                     double t = (x - p1.x) / (p2.x - p1.x);
-                    return p1.y + t * (p2.y - p1.x);
+                    return p1.y + t * (p2.x - p1.x);
                 }
             }
         }
         return Double.NaN;
     }
 
-    public void deletePoint(int index) {
-        points.remove(index);
-    }
-
-    public void addPoint(FunctionPoint point) {
-        int inpos = 0;
-        while (inpos < points.size() && points.get(inpos).x < point.x) {
-            inpos++;
-        }
-        points.add(inpos, point);
-        if (point.x < leftX_)
-            leftX_ = point.x;
-        if (point.x > rightX_)
-            rightX_ = point.x;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder("Tabulated Func:[");
-        for (int i = 0; i < points.size(); i++) {
-            sb.append(points.get(i).toString());
-            if (i < points.size() - 1)
-                sb.append(", ");
-        }
-        sb.append("]");
-        return sb.toString();
-    }
-
     public int getPointsCount() {
-        return points.size();
+        return points.length;
     }
 
     public FunctionPoint getPoint(int index) {
-        FunctionPoint p = new FunctionPoint(points.get(index));
+        FunctionPoint p = new FunctionPoint(points[index]);
         return p;
     }
 
     public void setPoint(int index, FunctionPoint point) {
-        if (index < 0 || index >= points.size()) {
-            throw new IndexOutOfBoundsException("Индекс вне допустимого диапазона");
+        if (index < 0 || index >= points.length) {
+            throw new IndexOutOfBoundsException("Индекс вне допустимого диапазона!");
         }
+
+        if (index > 0 && point.x <= points[index - 1].x) {
+            throw new IllegalArgumentException("Точка должна быть больше предыдущей!");
+        }
+
+        if (index < points.length - 1 && point.x > points[index + 1].x) {
+            throw new IllegalArgumentException("Точка должна быть меньше предыдущей!");
+        }
+
         FunctionPoint newp = new FunctionPoint(point);
-        points.set(index, newp);
+        points[index] = newp;
 
         if (index == 0) {
-            leftX_ = points.get(0).x;
+            leftX_ = points[0].x;
         }
 
-        if (index == points.size() - 1) {
-            rightX_ = points.get(points.size() - 1).x;
+        if (index == points.length - 1) {
+            rightX_ = points[points.length - 1].x;
         }
     }
-
+    
     public double getPointX(int index) {
-        return points.get(index).x;
+        FunctionPoint p = points[index];
+        return p.x;
     }
 
     public void setPointX(int index, double x) {
-        points.get(index).x = x;
+        if (index < 0 || index >= points.length) {
+            throw new IndexOutOfBoundsException("Индекс вне допустимого диапазона!");
+        }
+
+        if (index > 0 && x <= points[index - 1].x) {
+            throw new IllegalArgumentException("x должен быть больше предыдущей точки!");
+        }
+
+        if (index < points.length - 1 && x >= points[index + 1].x) {
+            throw new IllegalArgumentException("x должен быть меньше следующей точки!");
+        }
+
+        FunctionPoint newp = new FunctionPoint(x, points[index].y);
+        points[index] = newp;
+        points[index].x = x;
 
         if (index == 0) {
-            leftX_ = points.get(0).x;
+            leftX_ = points[0].x;
         }
-        if (index == points.size() - 1) {
-            rightX_ = points.get(points.size() - 1).x;
+        if (index == points.length - 1) {
+            rightX_ = points[points.length - 1].x;
         }
     }
 
     public double getPointY(int index) {
-        return points.get(index).y;
+        FunctionPoint p = new FunctionPoint(points[index]);
+        return p.y;
+    }
+    
+    public void setPointY(int index, double y) {
+        FunctionPoint newp = new FunctionPoint(points[index].x, y);
+        points[index] = newp;
     }
 
-    public void setPointY(int index, double y) {
-        points.get(index).y = y;
+    public void deletePoint(int index) {
+        FunctionPoint[] newarr = new FunctionPoint[points.length - 1];
+
+        System.arraycopy(points, 0, newarr, 0, index);
+        System.arraycopy(points, index + 1, newarr, index, points.length - 1 - index);
+
+        points = newarr;
+
+        leftX_ = points[0].x;
+        rightX_ = points[points.length - 1].x;
+    }
+    
+    public void addPoint(FunctionPoint point) {
+        
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Tabulated Func:[");
+        for (int i = 0; i < points.length; i++) {
+            sb.append(points[i].toString());
+            if (i < points.length - 1)
+                sb.append(", ");
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
