eb09dee8e179f68d53a4886e8971de76e7bdeb3a
Nomko <nomkopolopo@gmail.com>
2026-03-05 23:45:07 +0400

    переписал пару методов

--- first/packages/TabulatedFunction.java
+++ first/packages/TabulatedFunction.java
@@ -5,12 +5,14 @@ public class TabulatedFunction {
     private FunctionPoint points[];
     private int pointsCount_;
     private double leftX_, rightX_;
+    private int capacity;
 
     public TabulatedFunction(double leftX, double rightX, int pointsCount) {
         leftX_ = leftX;
         rightX_ = rightX;
         pointsCount_ = pointsCount;
-        points = new FunctionPoint[pointsCount_];
+        capacity = (pointsCount_ * 3)/2;
+        points = new FunctionPoint[capacity];
 
         double step = (rightX - leftX) / (pointsCount - 1);
         for (int i = 0; i < pointsCount_; i++) {
@@ -23,14 +25,14 @@ public class TabulatedFunction {
         leftX_ = leftX;
         rightX_ = rightX;
         pointsCount_ = values.length;
-        points = new FunctionPoint[pointsCount_];
+        capacity = (pointsCount_ * 3)/2;
+        points = new FunctionPoint[capacity];
 
         double step = (rightX - leftX) / (values.length - 1);
         for (int i = 0; i < pointsCount_; i++) {
             FunctionPoint p = new FunctionPoint(leftX + i * step, values[i]);
             points[i] = p;
         }
-        
     }
 
     public double getleftDomainBorder() {
@@ -56,7 +58,7 @@ public class TabulatedFunction {
     }
 
     public int getPointsCount() {
-        return points.length;
+        return pointsCount_;
     }
 
     public FunctionPoint getPoint(int index) {
@@ -133,24 +135,56 @@ public class TabulatedFunction {
         FunctionPoint[] newarr = new FunctionPoint[points.length - 1];
 
         System.arraycopy(points, 0, newarr, 0, index);
-        System.arraycopy(points, index + 1, newarr, index, points.length - 1 - index);
+        System.arraycopy(points, index + 1, newarr, index, pointsCount_ - index - 1);
 
         points = newarr;
+        pointsCount_--;
 
-        leftX_ = points[0].x;
-        rightX_ = points[points.length - 1].x;
+        if (pointsCount_ > 0) {
+            leftX_ = points[0].x;
+            rightX_ = points[pointsCount_-1].x;
+        }
     }
-    
+
+    //arr = {{1,2},{2,3},{2.5,1},{3,4}}
     public void addPoint(FunctionPoint point) {
-        
+        int index = rangeCheckForAdd(point.x);
+        if (pointsCount_ == points.length)
+            points = grow(); 
+
+        System.arraycopy(points, index, points, index + 1, pointsCount_ - index);
+
+        if (index < pointsCount_ && points[index].x == point.x) {
+            points[index] = point; // замена существующей
+            return;
+        }
+        points[index] = point;
+        ++pointsCount_;
+        // pointsCount_ = s + 1;
     }
     
+    private int rangeCheckForAdd(double x) {
+        for (int i = 0; i < pointsCount_; i++) {
+            if (x <= points[i].x) {
+                return i;
+            }
+        }
+        return pointsCount_;
+    }
+
+    private FunctionPoint[] grow() {
+        int newcap = (capacity * 3) / 2;
+        FunctionPoint[] newpPoints = new FunctionPoint[newcap];
+        System.arraycopy(points, 0, newpPoints, 0, pointsCount_);
+        return newpPoints;
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("Tabulated Func:[");
-        for (int i = 0; i < points.length; i++) {
+        for (int i = 0; i < pointsCount_; i++) {
             sb.append(points[i].toString());
-            if (i < points.length - 1)
+            if (i < pointsCount_ - 1)
                 sb.append(", ");
         }
         sb.append("]");
