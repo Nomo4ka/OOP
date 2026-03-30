a1c92181686d15ea40b2cae04b55093c577e5b37
Nomo4ka <nomkopolopo@gmail.com>
2026-03-30 10:00:43 +0400

    добавила новый интерфейс TabulatedFunction, который будет использоваться для создания табулированных функций. Этот интерфейс определяет методы для получения количества точек, получения координат точек и вычисления значения функции в заданной точке. Теперь можно реализовать этот интерфейс в классе LinkedListTabulatedFunction, чтобы создать табулированную функцию на основе связного списка.

---
+++ second/packages2/TabulatedFunction.java
@@ -0,0 +1,16 @@
+package packages2;
+
+public interface TabulatedFunction {
+    double getLeftDomainBorder();
+    double getRightDomainBorder();
+    double getFunctionalValue(double x);
+    int getPointsCount();
+    FunctionPoint getPoint(int index);
+    void setPoint(int index, FunctionPoint point);
+    double getPointX(int index);
+    void setPointX(int index, double x) throws InappropriateFunctionPointException;
+    double getPointY(int index);
+    void setPointY(int index, double y);
+    void deletePoint(int index);
+    void addPoint(FunctionPoint point) throws InappropriateFunctionPointException;
+}
